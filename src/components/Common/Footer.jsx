20d89e3de82d3efe58e6122787a491feb70a4452
Ignacio Lencina <nacholencina60@gmail.com>
2024-08-09 14:01:32 -0300

    Agrego layout base del header y footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-export default Footer
+    <footer className="mt-2 mb-3 text-center">
+      <p>Todos los derechos de Burger Tuc reservados &copy;</p>
+    </footer>
+  );
+};
+export default Footer;
