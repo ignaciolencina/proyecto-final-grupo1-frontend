656a905f4f2edbe8b9b7aa34410eafc4a9db49d9
xTucu6 <ivanbalce@gmail.com>
2024-08-20 17:10:41 -0300

    correcion del import de la card en la lista de productos

--- src/views/Lista-Productos/ProductListPage.jsx
+++ src/views/Lista-Productos/ProductListPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
-import ProductCard from '../Card/ProductCard';
+import ProductCard from '../../components/Card/ProductCard';
 
 const products = [
     {
