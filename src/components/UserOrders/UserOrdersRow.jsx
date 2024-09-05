d3b9d3075a140710da512bdf72fc4890b8300b23
Ignacio Lencina <nacholencina60@gmail.com>
2024-09-05 11:17:29 -0300

    Agrego colapse con productos por pedidos

--- src/components/UserOrders/UserOrdersRow.jsx
+++ src/components/UserOrders/UserOrdersRow.jsx
@@ -1,65 +1,60 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+
 import "./tableStyle.css";
 
 const UserOrdersRow = ({ orders, index }) => {
-  console.log(orders);
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const dateObject = new Date(orders.dateTime);
   const formattedDate = dateObject.toDateString().split("T")[0];
-  const modalId = `descriptionModal-${orders.orderId}`;
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <>
-      <tr>
+      <tr onClick={toggleExpand}>
         <td>{index + 1}</td>
         <td className="text-center">{formattedDate}</td>
         <td className="text-end">${orders.totalPrice}</td>
         <td className="text-end">
           <button
+            aria-controls={`collapseRow-${index}`}
+            aria-expanded={isExpanded ? "true" : "false"}
             className="detailButton"
-            data-bs-target={`#${modalId}`}
-            data-bs-toggle="modal"
+            data-bs-target={`#collapseRow-${index}`}
+            data-bs-toggle="collapse"
             type="button"
           >
-            Detalle
+            {isExpanded ? "Cerrar" : "Detalle"}
           </button>
         </td>
       </tr>
-      <div
-        aria-hidden="true"
-        aria-labelledby="foodDescription"
-        className="modal fade"
-        data-bs-theme="dark"
-        id={modalId}
-        tabIndex="-1"
+      <tr
+        className={`collapse ${isExpanded ? "show" : ""}`}
+        id={`collapseRow-${index}`}
       >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="foodDescription">
-                Pedido desde la mesa N°{orders.tableNumber}
-              </h1>
-              <button
-                aria-label="Close"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                type="button"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <p className="mt-3 mb-0 bodyFont"></p>
-            </div>
-            <div className="footer">
-              <h2 className="priceModal me-5">${orders.totalPrice}</h2>
-              <button
-                className="btn btn-outline-secondary"
-                data-bs-dismiss="modal"
-                type="button"
-              >
-                CERRAR
-              </button>
-            </div>
+        <td colSpan="4">
+          <div>
+            <p>
+              <strong>PRODUCTOS:</strong>
+            </p>
+            <ul>
+              {orders.products.map((product, i) => (
+                <li className="d-flex justify-content-between fst-italic" key={i}>
+                  {product.name} - (${product.price}) <span className="me-4 fst-normal">x {product.quantity}</span>
+                  
+                </li>
+              ))}
+            </ul>
+            <p>
+              <strong>Número de Mesa:</strong> {orders.tableNumber}
+            </p>
           </div>
-        </div>
-      </div>
+        </td>
+      </tr>
     </>
   );
 };
@@ -71,6 +66,7 @@ UserOrdersRow.propTypes = {
     totalPrice: PropTypes.string.isRequired,
     dateTime: PropTypes.string.isRequired,
     tableNumber: PropTypes.number.isRequired,
+    products: PropTypes.array.isRequired,
   }),
   index: PropTypes.number.isRequired,
 };
